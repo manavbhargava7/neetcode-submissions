55f70237ab1f6e38bc255d4b4aa36b85fbe5d271
Manav Bhargava <157171928+manavbhargava7@users.noreply.github.com>
2026-08-25 22:02:34 -0700

    Add: merge-intervals - submission-10

---
+++ Data Structures & Algorithms/merge-intervals/submission-10.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        /*
+        Implementation:
+
+        1. sort intervals by first number, if i2 n1 is less than or equal to i1n2, it overlaps, so then merge. continue for next
+
+        */
+
+        Arrays.sort(intervals, (i1, i2) -> Integer.compare(i1[0], i2[0]));
+
+        ArrayList<int[]> res = new ArrayList<>(); 
+
+        int i = 0;
+        while (i < intervals.length) {
+            while ((i < intervals.length - 1) && intervals[i + 1][0] <= intervals[i][1]) {
+                intervals[i + 1][0] = intervals[i][0];              
+                intervals[i + 1][1] = Math.max(intervals[i][1], intervals[i + 1][1]);             
+                i++;
+            } 
+            res.add(intervals[i]);
+            i++;
+        }
+
+        //convert to int[]
+        int[][] r = new int[res.size()][];
+
+        for (int j = 0; j < res.size(); j++) {
+            r[j] = res.get(j);
+        }
+
+        return r;
+
+
+    }
+}
