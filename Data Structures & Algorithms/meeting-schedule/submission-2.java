86ef1da339d97d599236e67eefa2ad5ae307d8b8
Manav Bhargava <157171928+manavbhargava7@users.noreply.github.com>
2026-08-25 23:24:57 -0700

    Add: meeting-schedule - submission-2

---
+++ Data Structures & Algorithms/meeting-schedule/submission-2.java
@@ -0,0 +1,25 @@
+/**
+ * Definition of Interval:
+ * public class Interval {
+ *     public int start, end;
+ *     public Interval(int start, int end) {
+ *         this.start = start;
+ *         this.end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+    public boolean canAttendMeetings(List<Interval> intervals) {
+        Collections.sort(intervals, (i1, i2) -> Integer.compare(i1.start, i2.start));
+
+        //check overlap
+        for (int i = 0; i < intervals.size() - 1; i++) {
+            if (intervals.get(i + 1).start < intervals.get(i).end) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
