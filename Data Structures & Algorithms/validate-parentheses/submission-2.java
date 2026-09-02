ce5c29f0991d57a1f98cebb8e761a08dda85253d
Manav Bhargava <157171928+manavbhargava7@users.noreply.github.com>
2026-09-02 12:28:30 -0700

    Add: validate-parentheses - submission-2

---
+++ Data Structures & Algorithms/validate-parentheses/submission-2.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isValid(String s) {
+        Deque<Character> stack = new ArrayDeque<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '{' || c == '(' || c == '[') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+
+                char last = stack.pop();
+                if (c == '}' && last != '{') {
+                    return false;
+                } else if (c == ']' && last != '[') {
+                    return false;
+                } else if (c == ')' && last != '(') {
+                    return false;
+                }
+            }
+        }
+
+        //gone through each value. now need to make sure all open brackets were indeed closed. 
+        return stack.isEmpty();
+    }
+}
