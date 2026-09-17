33be0e05918bdf23f0fac1ab23fbbc090782a58b
Manav Bhargava <157171928+manavbhargava7@users.noreply.github.com>
2026-09-17 01:12:21 -0700

    Add: valid-sudoku - submission-0

---
+++ Data Structures & Algorithms/valid-sudoku/submission-0.java
@@ -0,0 +1,61 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        ArrayList<Set<Character>> rows = new ArrayList<>();
+        ArrayList<Set<Character>> cols = new ArrayList<>();
+        ArrayList<Set<Character>> boxes = new ArrayList<>();
+        
+        for (int i = 0; i < 9; i++) {
+            rows.add(new HashSet<>());
+            cols.add(new HashSet<>());
+            boxes.add(new HashSet<>());
+        }
+
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                char val = board[i][j];
+                if (val == '.') {
+                    continue;
+                }
+                int bNum = getBoxNum(i, j);
+                if (rows.get(i).contains(val) || cols.get(j).contains(val) || boxes.get(bNum).contains(val)) {
+                    System.out.println();
+                    return false;
+                }
+
+                rows.get(i).add(val);
+                cols.get(j).add(val);
+                boxes.get(bNum).add(val);
+            }
+        }
+
+        return true;
+    }
+
+    public int getBoxNum(int i , int j) {
+        int iVal = Math.floorDiv(i, 3);
+        int jVal = Math.floorDiv(j, 3);
+
+        if (iVal == 0) {
+            if (jVal == 0) {
+                return 0;
+            } else if (jVal == 1) {
+                return 1;
+            }
+            return 2;
+        } else if (iVal == 1) {
+            if (jVal == 0) {
+                return 3;
+            } else if (jVal == 1) {
+                return 4;
+            }
+            return 5;
+        } else {
+            if (jVal == 0) {
+                return 6;
+            } else if (jVal == 1) {
+                return 7;
+            }
+            return 8;
+        }
+    }
+}
